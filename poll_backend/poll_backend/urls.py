42d4a59bcbe2ce7dd19b505697ddfe1751fc67e1
Tonnyderrick <tonnyaketch@gmail.com>
2025-08-01 17:24:06 +0300

    render

--- poll_backend/poll_backend/urls.py
+++ poll_backend/poll_backend/urls.py
@@ -1,21 +1,6 @@
-"""
-URL configuration for poll_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse  # 👈 add this import
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -30,9 +15,13 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+# 👇 add this simple root view
+def root_view(request):
+    return JsonResponse({"message": "Welcome to the Poll API"})
+
 urlpatterns = [
+    path('', root_view),  # 👈 handles /
     path('admin/', admin.site.urls),
     path('api/', include('polls.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
-
